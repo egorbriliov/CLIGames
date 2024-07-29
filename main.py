9d8f7afbeec4ab7d4da1e83bc2eb195eead2d167
Егор Брилёв <egor.briliov@gmai.com>
2024-07-29 20:46:39 +0300

    Инкапсулировал очистку консоли с помощью декораторов

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 import importlib.util
 import os
 
+from app.decorators import cls_clear
+from app.functions import show_get_menu
+
 directory_with_games = 'games'
 
 
@@ -13,42 +16,29 @@ class CliApp:
     def __init__(self):
         self.games_manager = GamesManager()
 
+    @cls_clear(time_to_sleep=1)
     def start(self, parameter=None):
         """
         Метод позволяет выбирать настройки самого приложения:
         Настройки представлены: dict с методами вызова настроек
         """
-        parameters_dict = [
+        parameters_list = [
             {"index": 1, "name": "Выбрать игру", "method": self.games_manager}
         ]
 
         if parameter is None:
-            os.system('cls||clear')
-            print("\n\nДобро пожаловать в меню программы, "
-                  "\nчем желаете заняться?")
 
-            print()
-            for parameter in parameters_dict:
-                print(f"{parameter["index"]}: {parameter["name"]}")
-
-            def parameter_get():
-                print()
-                parameter = int(input(f"Введите номер параметра (от {1} до {len(parameters_dict)}): "))
-                if parameter in range(1, len(parameters_dict) + 1):
-                    return parameter
-                else:
-                    print("Было введено неверное значение!")
-                    parameter_get()
-
-            parameter = parameter_get()
+            parameter = show_get_menu(len_parameters=len(parameters_list),
+                                      list_to_show=parameters_list,
+                                      menu_bio="\n\n\033[5;1mДобро пожаловать в меню программы, "
+                                               "\nчем желаете заняться?\033[0m\n")
             self.start(parameter=parameter)
 
         else:
             method = [parameter_in_dict["method"]
-                      for parameter_in_dict in parameters_dict
+                      for parameter_in_dict in parameters_list
                       if parameter_in_dict["index"] == parameter][0]
-            start = getattr(method, "start")
-            start()
+            getattr(method, "start")()
 
 
 class GamesManager:
@@ -64,40 +54,29 @@ class GamesManager:
             if os.path.isfile(filepath):
                 games_list.append({
                     "index": len(games_list) + 1,
-                    "file_name": filename,
-                    "file_path": filepath,
+                    "name": filename[:-3],
+                    "path": filepath,
                 })
 
         return games_list
 
+    @cls_clear(time_to_sleep=1)
     def start(self):
-        os.system('cls||clear')
-        print("\n\nВы перешли в меню выбора игры!\n")
 
-        for game in self.games_list:
-            print(f"{game["index"]}: {game["file_name"][:-3]}")
-
-        def game_index_get():
-            print()
-            game_number = int(input(f"Введите номер игры, которую желаете запустить"
-                                    f"\n(от {1} до {len(self.games_list)}): "))
-            if game_number in range(1, len(self.games_list) + 1):
-                return game_number
-            else:
-                print("Было введено неверное значение!")
-                game_index_get()
-
-        game_index = game_index_get()
+        game_index = show_get_menu(len_parameters=len(self.games_list),
+                                   list_to_show=self.games_list,
+                                   menu_bio="\n\n\033[5;1mВы перешли в меню выбора игры!\n\033[0m")
         self.start_game(game_index)
 
+    @cls_clear(time_to_sleep=1)
     def start_game(self, game_index):
 
-        game_file_path = [parameter_in_dict["file_path"]
+        game_file_path = [parameter_in_dict["path"]
                           for parameter_in_dict in self.games_list
                           if parameter_in_dict["index"] == game_index][0]
-        game_name = [parameter_in_dict["file_name"]
+        game_name = [parameter_in_dict["name"]
                      for parameter_in_dict in self.games_list
-                     if parameter_in_dict["index"] == game_index][0][:-3]
+                     if parameter_in_dict["index"] == game_index][0]
 
         def import_module_from_path(path):
             spec = importlib.util.spec_from_file_location("my_module", path)
@@ -105,8 +84,7 @@ class GamesManager:
             spec.loader.exec_module(module)
             return module
 
-        os.system('cls||clear')
-        print(f"\n\nЗапуск игры: {game_name}")
+        print(f"\033[5;1m\n\nЗапуск игры: {game_name}\033[0m")
         start = getattr(import_module_from_path(game_file_path), "start")
         start()
 
